7b74cd5e97be5f563410ab170654be51d80a6633
sansan296 <sanshizu429@gmail.com>
2025-10-14 18:18:31 +0900

    タグ機能うまくいかない

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,10 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        
+        <script src="//unpkg.com/alpinejs" defer></script>
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -37,5 +41,6 @@
 
             </main>
         </div>
+
     </body>
 </html>
